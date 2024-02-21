8d375399c6cca4b71bbe818af580d71d06165fe4
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2024-02-20 20:19:29 -0600

    Add password generator functionality

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const pwEl = document.getElementById("pw");
+const copyEl = document.getElementById("copy");
+const lenEl = document.getElementById("len");
+const upperEl = document.getElementById("upper");
+const lowerEl = document.getElementById("lower");
+const numberEl = document.getElementById("number");
+const symbolEl = document.getElementById("symbol");
+const generateEl = document.getElementById("generate");
+
 let upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let lowerLetters = "abcdefghijklmnopqrstuvwxyz";  
 let numbers = "0123456789";
